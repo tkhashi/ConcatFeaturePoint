c7be2a983b34d1155f13a1b0c8bf186c44252ce4
KazuhiroTakahashi <k_takahashi@just-ltd.co.jp>
2023-01-23 19:12:15 +0900

    Detect feature and concat goodmatches

---
+++ ConcatImages/ConcatService.cs
@@ -0,0 +1,57 @@
+﻿using OpenCvSharp;
+using System.Reactive.Linq;
+
+namespace ConcatImages;
+
+public class ConcatService
+{
+    public void Concat(string videoPath)
+    {
+        var cap = VideoCapture.FromFile(videoPath);
+        using var akaze = AKAZE.Create();
+        using var matcher = new BFMatcher(NormTypes.Hamming);
+        Observable.Range(0, cap.FrameCount - 1)
+            .Select(i =>
+            {
+                cap.PosMsec = i * 500;
+                return cap.RetrieveMat();
+            })
+            .Aggregate((ele, next) =>
+            {
+                // 特徴点を検出
+                // キーポイントを検出
+                using var descriptors1 = new Mat();
+                using var descriptors2 = new Mat();
+                akaze.DetectAndCompute(ele, null, out var keyPoints1, descriptors1);
+                akaze.DetectAndCompute(next, null, out var keyPoints2, descriptors2);
+
+                // それぞれの特徴量をマッチング
+                var matches = matcher.Match(descriptors1, descriptors2);
+                if (matches.Length == 0) return ele;
+                // キーポイントの情報で最も距離の近い（似ている）キーポイントを見る。
+                var goodMatch = matches.MinBy(x => x.Distance);
+                //using var drawMatch = new Mat();
+                //Cv2.DrawMatches(ele, keyPoints1, next, keyPoints2, new[] { goodMatch }, drawMatch);
+                //Cv2.ImShow("image match", drawMatch);
+                //Cv2.WaitKey();
+
+                var x1 = keyPoints1.ElementAt(goodMatch.QueryIdx).Pt.X;
+                var x2 = keyPoints2.ElementAt(goodMatch.TrainIdx).Pt.X;
+                var trimmedEle = ele.Clone(new Rect(0, 0, (int) x1, ele.Height));
+                var trimmedNext = next.Clone(new Rect((int) x2, 0, next.Width - (int) x2, next.Height));
+                var dst = new Mat();
+                Cv2.HConcat(new[] {trimmedEle, trimmedNext}, dst);
+                //Cv2.ImShow("concat", dst);
+                //Cv2.WaitKey();
+
+                return dst;
+            })
+            .Subscribe(mat =>
+            {
+                Cv2.ImShow("concat?", mat);
+                Cv2.WaitKey();
+            });
+
+        return;
+    }
+}
--- ConcatImages/Program.cs
+++ ConcatImages/Program.cs
@@ -1,152 +1,158 @@
-﻿using OpenCvSharp;
-using System.Diagnostics;
+﻿using ConcatImages;
 
-using var right = new Mat(@"Resources/right.jpg");
-using var center = new Mat(@"Resources/center.jpg");
-using var left = new Mat(@"Resources/left.jpg");
-using var full = new Mat(@"Resources/full.jpg");
+const string videoPath = @"C:\Users\Kazuhiro Takahashi\Desktop\roofInspection\DJI_0299.MP4";
+var concater = new ConcatService();
+concater.Concat(videoPath);
 
-//Hoge(right, center);
-//Stitch(center, full);
-var trims = TrimStrip(full, 50, 10);
-using var r = Stitch(trims.ToArray());
-Cv2.ImShow("result", r);
-Cv2.WaitKey();
+//using OpenCvSharp;
+//using System.Diagnostics;
 
-Mat Stitch(params Mat[] param)
-{
-    var seed = param[0];
-    param.ToList().RemoveAt(0);
-    var result = param.Aggregate(seed, (ele, next) => StitchPair(ele, next));
+//using var right = new Mat(@"Resources/right.jpg");
+//using var center = new Mat(@"Resources/center.jpg");
+//using var left = new Mat(@"Resources/left.jpg");
+//using var full = new Mat(@"Resources/full.jpg");
 
-    //Cv2.ImShow("pair", result);
-    //Cv2.WaitKey();
-    return result;
-}
+////Hoge(right, center);
+////Stitch(center, full);
+//var trims = TrimStrip(full, 50, 10);
+//using var r = Stitch(trims.ToArray());
+//Cv2.ImShow("result", r);
+//Cv2.WaitKey();
 
-Mat StitchPair(Mat one, Mat two)
-{
-    Cv2.ImShow("one", one);
-    Cv2.WaitKey();
-    Cv2.ImShow("two", two);
-    Cv2.WaitKey();
-    var inputs = new List<Mat>() { one, two};
-    var resultPanorama = new Mat();
-    var stitcher = Stitcher.Create(Stitcher.Mode.Scans);
-    var status = stitcher.Stitch(inputs, resultPanorama);
+//Mat Stitch(params Mat[] param)
+//{
+//    var seed = param[0];
+//    param.ToList().RemoveAt(0);
+//    var result = param.Aggregate(seed, (ele, next) => StitchPair(ele, next));
 
-    if (status is not Stitcher.Status.OK) return two;
-    //Cv2.ImShow("pair", resultPanorama);
-    //Cv2.WaitKey();
-    return resultPanorama;
-}
+//    //Cv2.ImShow("pair", result);
+//    //Cv2.WaitKey();
+//    return result;
+//}
 
-IEnumerable<Mat> TrimStrip(Mat img, int equiDistant, int overrapWidth)
-{
-    var imgFullWidth = img.Width;
-    var trimWidth = imgFullWidth / equiDistant + (imgFullWidth / (equiDistant * overrapWidth));
+//Mat StitchPair(Mat one, Mat two)
+//{
+//    Cv2.ImShow("one", one);
+//    Cv2.WaitKey();
+//    Cv2.ImShow("two", two);
+//    Cv2.WaitKey();
+//    var inputs = new List<Mat>() { one, two};
+//    var resultPanorama = new Mat();
+//    var stitcher = Stitcher.Create(Stitcher.Mode.Scans);
+//    var status = stitcher.Stitch(inputs, resultPanorama);
 
-    var trims = Enumerable.Range(0, equiDistant)
-        .Select(i => 
-        {
-            var startX = i * (imgFullWidth / equiDistant);
-            var w = i * (imgFullWidth / equiDistant) + trimWidth > imgFullWidth
-                ? imgFullWidth - startX
-                : trimWidth;
-            var trimed = img.Clone(new Rect(startX, 0, w, img.Height));
-            return trimed;
-        })
-        .ToArray();
+//    if (status is not Stitcher.Status.OK) return two;
+//    //Cv2.ImShow("pair", resultPanorama);
+//    //Cv2.WaitKey();
+//    return resultPanorama;
+//}
 
-    //foreach(var trimmed in trims)
-    //{
-    //    Cv2.ImShow("trimmed", trimmed);
-    //    Cv2.WaitKey();
-    //}
+//IEnumerable<Mat> TrimStrip(Mat img, int equiDistant, int overrapWidth)
+//{
+//    var imgFullWidth = img.Width;
+//    var trimWidth = imgFullWidth / equiDistant + (imgFullWidth / (equiDistant * overrapWidth));
 
-    return trims;
-}
+//    var trims = Enumerable.Range(0, equiDistant)
+//        .Select(i => 
+//        {
+//            var startX = i * (imgFullWidth / equiDistant);
+//            var w = i * (imgFullWidth / equiDistant) + trimWidth > imgFullWidth
+//                ? imgFullWidth - startX
+//                : trimWidth;
+//            var trimed = img.Clone(new Rect(startX, 0, w, img.Height));
+//            return trimed;
+//        })
+//        .ToArray();
 
-void StitchVideoFrame(string videoPath)
-{
-    Mat result = null;
-    using var capture = VideoCapture.FromFile(videoPath);
-    for (int i = 0; i < capture.FrameCount - 1; i++)
-    {
-        capture.PosFrames = i;
-        using var mat = capture.RetrieveMat();
-        // TODO: unfold
-        // using var unfolded = Unfold(mat);
-        if (result is null)
-        {
-            // result = unfolded;
-            result = mat;
-            continue;
-        }
+//    //foreach(var trimmed in trims)
+//    //{
+//    //    Cv2.ImShow("trimmed", trimmed);
+//    //    Cv2.WaitKey();
+//    //}
 
-        //result = Stitch(result, unfolded);
-        result = Stitch(result, mat);
-    }
-    Cv2.ImShow("stitched", result);
-    Cv2.WaitKey();
-}
+//    return trims;
+//}
 
-void Hoge(Mat s1, Mat s2)
-{
-    var src = new Mat[2];
-    var dst = new Mat[2];
-    using var outImg = new Mat();
-    using var descripter1 = new Mat();
-    using var descripter2 = new Mat();
-    using var result = new Mat();
+//void StitchVideoFrame(string videoPath)
+//{
+//    Mat result = null;
+//    using var capture = VideoCapture.FromFile(videoPath);
+//    for (int i = 0; i < capture.FrameCount - 1; i++)
+//    {
+//        capture.PosFrames = i;
+//        using var mat = capture.RetrieveMat();
+//        // TODO: unfold
+//        // using var unfolded = Unfold(mat);
+//        if (result is null)
+//        {
+//            // result = unfolded;
+//            result = mat;
+//            continue;
+//        }
 
-    src[0] = s1;
-    src[1] = s2;
-    dst[0] = new Mat();
-    dst[1] = new Mat();
-    Cv2.CvtColor(src[0], dst[0], ColorConversionCodes.BGR2GRAY);
-    Cv2.CvtColor(src[1], dst[1], ColorConversionCodes.BGR2GRAY);
+//        //result = Stitch(result, unfolded);
+//        result = Stitch(result, mat);
+//    }
+//    Cv2.ImShow("stitched", result);
+//    Cv2.WaitKey();
+//}
 
-    var akaze = AKAZE.Create();
-    akaze.DetectAndCompute(dst[0], null, out KeyPoint[] keypoints1, descripter1);
-    akaze.DetectAndCompute(dst[1], null, out KeyPoint[] keypoints2, descripter2);
+//void Hoge(Mat s1, Mat s2)
+//{
+//    var src = new Mat[2];
+//    var dst = new Mat[2];
+//    using var outImg = new Mat();
+//    using var descripter1 = new Mat();
+//    using var descripter2 = new Mat();
+//    using var result = new Mat();
 
-    var matcher = new BFMatcher();
-    var matches = matcher.Match(descripter1, descripter2);
+//    src[0] = s1;
+//    src[1] = s2;
+//    dst[0] = new Mat();
+//    dst[1] = new Mat();
+//    Cv2.CvtColor(src[0], dst[0], ColorConversionCodes.BGR2GRAY);
+//    Cv2.CvtColor(src[1], dst[1], ColorConversionCodes.BGR2GRAY);
 
-    Cv2.DrawMatches(src[0], keypoints1, src[1], keypoints2, matches, outImg);
+//    var akaze = AKAZE.Create();
+//    akaze.DetectAndCompute(dst[0], null, out KeyPoint[] keypoints1, descripter1);
+//    akaze.DetectAndCompute(dst[1], null, out KeyPoint[] keypoints2, descripter2);
 
-    Cv2.ImShow("outimg", outImg);
-    Cv2.WaitKey();
+//    var matcher = new BFMatcher();
+//    var matches = matcher.Match(descripter1, descripter2);
 
-    //画像合体
-    var size = matches.Length;
-    var getPoints1 = new Vec2f[size];
-    var getPoints2 = new Vec2f[size];
+//    Cv2.DrawMatches(src[0], keypoints1, src[1], keypoints2, matches, outImg);
 
-    for (var a = 0; a < size; a++)
-    {
-        var pt1 = keypoints1[matches[a].QueryIdx].Pt;
-        var pt2 = keypoints2[matches[a].TrainIdx].Pt;
-        getPoints1[a][0] = pt1.X;
-        getPoints1[a][1] = pt1.Y;
-        getPoints2[a][0] = pt2.X;
-        getPoints2[a][1] = pt2.Y;
-        Debug.WriteLine($"{pt2.X - pt1.X}, {pt2.Y - pt1.Y}");
-    }
+//    Cv2.ImShow("outimg", outImg);
+//    Cv2.WaitKey();
 
-    var hom = Cv2.FindHomography(InputArray.Create(getPoints1), InputArray.Create(getPoints2), HomographyMethods.Ransac);
+//    //画像合体
+//    var size = matches.Length;
+//    var getPoints1 = new Vec2f[size];
+//    var getPoints2 = new Vec2f[size];
 
-    Cv2.WarpPerspective(src[0], result, hom, new Size(src[0].Cols * 2.0, src[0].Rows * 2.0));
-    for (int y = 0; y < src[0].Rows; y++)
-    {
-        for (int x = 0; x < src[0].Cols; x++)
-        {
-            result.Set(y, x, src[1].At<Vec3b>(y, x));
-        }
-    }
+//    for (var a = 0; a < size; a++)
+//    {
+//        var pt1 = keypoints1[matches[a].QueryIdx].Pt;
+//        var pt2 = keypoints2[matches[a].TrainIdx].Pt;
+//        getPoints1[a][0] = pt1.X;
+//        getPoints1[a][1] = pt1.Y;
+//        getPoints2[a][0] = pt2.X;
+//        getPoints2[a][1] = pt2.Y;
+//        Debug.WriteLine($"{pt2.X - pt1.X}, {pt2.Y - pt1.Y}");
+//    }
 
-    Cv2.ImShow("result", result);
-    Cv2.WaitKey();
-}
+//    var hom = Cv2.FindHomography(InputArray.Create(getPoints1), InputArray.Create(getPoints2), HomographyMethods.Ransac);
+
+//    Cv2.WarpPerspective(src[0], result, hom, new Size(src[0].Cols * 2.0, src[0].Rows * 2.0));
+//    for (int y = 0; y < src[0].Rows; y++)
+//    {
+//        for (int x = 0; x < src[0].Cols; x++)
+//        {
+//            result.Set(y, x, src[1].At<Vec3b>(y, x));
+//        }
+//    }
+
+//    Cv2.ImShow("result", result);
+//    Cv2.WaitKey();
+//}
 
